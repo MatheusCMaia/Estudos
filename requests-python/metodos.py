b462fb0589b63b0306225097c3de9d44cf49593f
Matheus Augusto <matheusaugustotrabalho123@gmail.com>
2026-09-08 18:13:21 -0300

    Estudos Argparse

--- requests-python/metodos.py
+++ requests-python/metodos.py
@@ -8,21 +8,21 @@ requisicao_moedas = requests.get('https://economia.awesomeapi.com.br/last/USD-BR
 #print(requisicao_moedas.json())
 #criar banco de dados no firebase
 #firebase utiliza .json também
-requisicao_firebase = requests.get('https://teste-ab7c7-default-rtdb.firebaseio.com/.json')
+requisicao_firebase = requests.get('link do seu firebase (tirei por motivos de segurança)')
 #print(requisicao_firebase.json())
 #metodo post
 #precisa passar um DATA para ser colocado no banco de dados
 #pegando informações das moedas e colocando em uma variavel
 informacoes_moedas = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
 #colocando as informaçõpes da moeda no firebase
-post_firebase = requests.post('https://teste-ab7c7-default-rtdb.firebaseio.com/.json', data=informacoes_moedas)
+post_firebase = requests.post('link do seu firebase (tirei por motivos de segurança)', data=informacoes_moedas)
 #alterar algo no banco de dados
-dados_1 = '{"Dinheiro": "R$999,9", "Idade": "23", "Nome": "Matheus Augusto"}'
+dados_1 = '{"Dinheiro": "R$999,9", "Idade": "23", "Nome": "Teste"}'
 #metodo patch
-patch_fire = requests.patch('https://teste-ab7c7-default-rtdb.firebaseio.com/1.json', data=dados_1)
+patch_fire = requests.patch('link do seu firebase (tirei por motivos de segurança)', data=dados_1)
 print(patch_fire)
 #sempre colocar o .json no final
-alterar_informacoes = requests.patch('https://teste-ab7c7-default-rtdb.firebaseio.com/-P0Tsip1827JCkI-Zjz4.json', data=informacoes_moedas)
+alterar_informacoes = requests.patch('link do seu firebase (tirei por motivos de segurança)', data=informacoes_moedas)
 #metodo delete
-delete_fire = requests.delete('https://teste-ab7c7-default-rtdb.firebaseio.com/-P0Tsip1827JCkI-Zjz4.json')
+delete_fire = requests.delete('link do seu firebase (tirei por motivos de segurança)')
 print(delete_fire)
